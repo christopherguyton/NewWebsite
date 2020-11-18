b901d83b7d25f2a1c8cf01317e83db9452464a40
MrBehavior <cguyton87@gmail.com>
2020-11-18 12:48:13 -0800

    MVP Functionality Complete

--- src/pages/BioPage.js
+++ src/pages/BioPage.js
@@ -6,8 +6,9 @@ import {Container, Row, Col} from 'react-bootstrap'
 
 //Custom
 import ChibiLocs from '../images/ChibiLocs.png'
-import ClickArrow from '../components/ClickArrow'
-import BioComponent from '../components/BioComponent'
+import RealLocs from '../images/ChrisReal.jpg'
+import CartoonLocs from '../images/ChrisFace2.PNG'
+
 
 function BioPage(props) {
     return (
@@ -17,16 +18,36 @@ function BioPage(props) {
         <h3 className="subtitle">{props.subtitle}</h3>
         <Row> 
         <Col>
-        <img src={ChibiLocs} style={{width: 400}} alt="ChibiLocs"></img>
+        <img src={ChibiLocs} style={{width: 400}} alt="ChibiLocs" id="ChibiLocs"></img>
         </Col>
         <Col>
-        <BioComponent/>
-        <ClickArrow/>
+        <h5 className="bioSection">{props.section1}</h5>
+        </Col>
+        </Row>
+        </Container>
+        <Container fluid className="SpacedContainer">
+        <Row> 
+        <Col>
+        <h5 className="bioSection2">{props.section2}</h5>
+        </Col>
+        <Col>
+        <img src={RealLocs} style={{width: 400}} alt="RealLocs" id="RealLocs"></img>
+        </Col>
+       
+        </Row>
+
+        </Container>
+        <Container fluid className="SpacedContainer">
+        <Row> 
+        <Col>
+        <img src={CartoonLocs} style={{width: 300}} alt="CartoonLocs" id="CartoonLocs"></img>
+        </Col>
+        <Col>
+        <h5 className="bioSection3">{props.section3}</h5>
         </Col>
         </Row>
 
         </Container>
-    
         </div>
     )
 }
--- src/pages/Support.js
+++ src/pages/Support.js
@@ -5,27 +5,29 @@ import {Container, Row, Col} from 'react-bootstrap';
 
 import PatreonLogo from '../images/PatreonLogo.png'
 import VenmoLogo from '../images/VenmoLogo.png'
+import PayPalLogo from '../images/PayPal.png'
 
 export default function Support() {
     return (
        
         <div>
-        <Container className="HeroContainer">
+        <Container className="HeroContainer" style={{padding: 50}}>
         <Row>
         <Col>
         <Container>
-        <h2 className="description" style={{textAlign: 'center', paddingTop: 10}}>Like the Content and Want to Support?</h2>
+        <h2 className="description" style={{textAlign: 'center'}}>Like the Content and Want to Support?</h2>
         </Container>
         </Col>
         </Row>
         <Row>
         <Col>
-        <h3 className="description" style={{marginLeft: 100, paddingTop: 10}}>Become a Patron...</h3>
-        <a href="https://www.patreon.com/mrbehavior"><img src={PatreonLogo} alt="patreonlogo" className="logos" style={{width: 100, marginLeft: 150, paddingBottom: 10}}/></a>
+        <h3 className="description" style={{marginLeft: 100, paddingTop: 10,}}>Become a Patron...</h3>
+        <a href="https://www.patreon.com/mrbehavior"><img src={PatreonLogo} alt="patreonlogo" className="logos" style={{width: 100, marginLeft: 150, paddingBottom: 10, backgroundColor: 'black'}}/></a>
         </Col>
         <Col>
         <h3 className="description" style={{marginLeft: 100, paddingTop: 10}}>Or Donate Directly...</h3>
-        <a href="https://venmo.com/Chris-Locs2"><img src={VenmoLogo} alt="venmologo" className="logos" style={{width: 100, marginLeft: 160, paddingBottom: 10}}/></a>
+        <a href="https://venmo.com/Chris-Locs2"><img src={VenmoLogo} alt="venmologo" className="logos" style={{width: 100, marginLeft: 70, paddingBottom: 10}}/></a>
+        <a href="https://paypal.me/mrbehavior"><img src={PayPalLogo} alt="paypallogo" className="logos" style={{width: 100, marginLeft: 100, paddingBottom: 10}}/></a>
         </Col>
 
         </Row>
