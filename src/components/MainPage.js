4edde253c3d635b8bf27c0f15f5ce2b5c22b5529
Christopher Guyton <christopherguyton87@gmail.com>
2020-12-10 23:54:06 -0800

    spaced out the social links more

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -24,13 +24,13 @@ function MainPage(props) {
         <h2 className="description">{props.description}</h2>
         <Container className="LogoContainer">
         <Row>
-        <a href="https://open.spotify.com/show/6KA6SqohjUvrrzlZDjIXuL?si=3P7glcofQ3iIZ7r8sf6oQQ"><img src={SpotifyLogo} alt="spotify" thumbnail style={{width: 110, height: 90}} className="logos"/></a>
-        <a href="https://www.youtube.com/channel/UCAegArzsmQ15lOKgSOU-ZHg?view_as=subscriber"><img src={YouTubeLogo} alt="youtube" thumbnail style={{width: 100, marginTop: 5, marginLeft: 70}} className="logos"/></a>
-        <a href="https://www.twitch.tv/mrbehavior"><img src={TwitchLogo} alt="spotify" thumbnail style={{width: 110, height: 90, marginLeft: 70}} className="logos"/></a>
+        <a href="https://open.spotify.com/show/6KA6SqohjUvrrzlZDjIXuL?si=3P7glcofQ3iIZ7r8sf6oQQ"><img src={SpotifyLogo} alt="spotify" thumbnail style={{width: 90, height: 90}} className="logos" id="spotify"/></a>
+        <a href="https://www.youtube.com/channel/UCAegArzsmQ15lOKgSOU-ZHg?view_as=subscriber"><img src={YouTubeLogo} alt="youtube" thumbnail style={{width: 90, marginTop: 5, marginLeft: 70}} className="logos"/></a>
+        <a href="https://www.twitch.tv/mrbehavior"><img src={TwitchLogo} alt="spotify" thumbnail style={{width: 100, height: 90, marginLeft: 70}} className="logos"/></a>
        
         <div className="rowTwoLogos">
-        <a href="https://twitter.com/RealMrBehavior"><img src={TwitterLogo} alt="twitter" thumbnail style={{width: 110, height: 90}} className="logos"/></a>
-        <a href="https://misterbehavior.bandcamp.com/album/playlist-x"><img src={BandCampLogo} alt="bandcamp" thumbnail style={{width: 110, height: 90, marginLeft: 70}} className="logos"/></a>
+        <a href="https://twitter.com/RealMrBehavior"><img src={TwitterLogo} alt="twitter" thumbnail style={{width: 90, height: 90}} className="logos"/></a>
+        <a href="https://misterbehavior.bandcamp.com/album/playlist-x"><img src={BandCampLogo} alt="bandcamp" thumbnail style={{width: 100, height: 90, marginLeft: 70}} className="logos"/></a>
         </div>
         </Row>
      
