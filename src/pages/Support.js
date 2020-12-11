5f93d98849d8eab38a0782af9b0b1baaa7ecac80
Christopher Guyton <christopherguyton87@gmail.com>
2020-12-11 12:40:22 -0800

    adjusted support page

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Container, Row, Col} from 'react-bootstrap';
 
+
 //Custom
 
 import PatreonLogo from '../images/PatreonLogo.png'
@@ -19,18 +20,29 @@ export default function Support() {
         </Container>
         </Col>
         </Row>
+       <Row>
+       <h3 className="description" style={{}}>Become a Patron...</h3>
+    
+    <Col>
+    <a href="https://www.patreon.com/mrbehavior"><img src={PatreonLogo} alt="patreonlogo" className="appearances" style={{width: 100, marginLeft: 150, paddingBottom: 10, backgroundColor: 'black'}}/></a>
+        </Col>
+    </Row>
         <Row>
-        <Col>
-        <h3 className="description" style={{marginLeft: 100, paddingTop: 10,}}>Become a Patron...</h3>
-        <a href="https://www.patreon.com/mrbehavior"><img src={PatreonLogo} alt="patreonlogo" className="appearances" style={{width: 100, marginLeft: 150, paddingBottom: 10, backgroundColor: 'black'}}/></a>
-        </Col>
-        <Col>
-        <h3 className="description" style={{marginLeft: 100, paddingTop: 10}}>Or Donate Directly...</h3>
-        <a href="https://venmo.com/Chris-Locs2"><img src={VenmoLogo} alt="venmologo" className="appearances" style={{width: 100, marginLeft: 70, paddingBottom: 10}}/></a>
-        <a href="https://paypal.me/mrbehavior"><img src={PayPalLogo} alt="paypallogo" className="appearances" style={{width: 100, marginLeft: 100, paddingBottom: 10}}/></a>
-        </Col>
-
+        <Row>
+        
         </Row>
+        <Col>
+        <h3 className="description" style={{}}>Or Donate Directly...</h3>
+        </Col>
+        <Col>
+        <a href="https://venmo.com/Chris-Locs2"><img src={VenmoLogo} alt="venmologo" className="appearances" style={{width: 100, paddingTop: 10, marginRight: 10}}/></a>
+        <a href="https://paypal.me/mrbehavior"><img src={PayPalLogo} alt="paypallogo" className="appearances" style={{width: 100, paddingTop: 10}}/></a>
+       
+        </Col>
+        </Row>
+       
+
+      
        
         </Container>
          
